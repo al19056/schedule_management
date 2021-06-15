45fff9ae60413f4aa2731ca348270a1ba5455197
AL19116 <84761454+AL19116@users.noreply.github.com>
2021-06-15 22:56:34 +0900

    Update C7.py

--- C7.py
+++ C7.py
@@ -5,13 +5,22 @@
 ##################################################
 
 #CustomerInformationRegistration 顧客情報登録
+from typing import Counter
+import os
+
+
+userIDlist = []
+passwordlist = []
+#userIDlist : userIDの配列
+#passwordlist : passwordの配列 
+# ここに各々のデータを格納する
+
+
+
 def CIR(userID,password):
     import sys
     import math
     import argparse
-    import logging
-    global userIDlist 
-    global passwordlist
     #userIDlist : userIDの配列
     #passwordlist : passwordの配列 
     # ここに各々のデータを格納する 
@@ -22,14 +31,38 @@ def CIR(userID,password):
             password (str):W1ログイン画面からPOSTされたpasswordフィールド
 
 
-    """        
-    count = 0
-    userIDlist = []
-    passwordlist = []
-    userIDlist.append("userID")
-    passwordlist.append("password")
-    count+=1
-    if userIDlist[count] is None:
+    """
+    #userIDとpasswordを各々のlistに追加する
+    userIDlist.append(userID)
+    passwordlist.append(password)
+    
+    """
+    file1.txt ----> userIDのデータを保存する
+    file2.txt ----> passwordのデータを保存する
+    """
+
+    #file1.txtを追記状態で開き、userIDを追加する
+    f1 = open("file1.txt","a")
+    f1.write(userID)
+    f1.write("\n")
+    f1.close
+    #file2.txtが存在しない場合
+    if os.path.exists('file2.txt') is False:
+        #file2.txtを新規書き込み状態で開き、passwordを追加する
+        f2 = open("file2.txt","w")
+        f2.write(password)
+        f2.write("\n")
+        f2.close
+    #file2.txtが存在する場合
+    else:
+        #file2.txtを追記状態で開き、passwordを追加する
+        f2 = open("file2.txt","a")
+        f2.write(password)
+        f2.write("\n")
+        f2.close
+
+    #先ほど入れた配列の中身があるかどうか調べる
+    if userIDlist[0] is None:
         return 0
     else:
         return 1
@@ -41,7 +74,19 @@ def CISu():
     userIDの情報をC2 認証処理部にデータを送る
     戻り値　:　userIDlist
     """
-    return userIDlist
+    #file1.txtが存在しない場合
+    if os.path.exists('file1.txt') is False:
+        #file1.txtを作り、その後読み込み状態にする
+        f3 = open("file1.txt","w")
+        f3.close()
+        f4 = open("file1.txt","r")
+        return f4.readlines()      #f4をlist型の状態で返す
+    #file1.txtが存在する場合
+    else:
+        #file1.txtを読み込み状態にする    
+        f3 = open("file1.txt","r")
+        return f3.readlines()      #f3をlist型の状態で返す
+
 
 #CustomerInformationSuvey(password) 顧客情報調査(password)
 def CISp():
@@ -49,6 +94,9 @@ def CISp():
     passwordの情報をC2 認証処理部にデータを送る
     戻り値: passwordlist
     """
-    return passwordlist
+        
+    #最初からfile2を読み込み状態にする
+    f5 = open("file2.txt","r")
+    return f5.readlines()      #f5をlist型の状態で返す
+        
 
-    
