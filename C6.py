15204eac21543eec5633d90c4d4f7d25a75a2683
Ara0921 <84761127+Ara0921@users.noreply.github.com>
2021-06-18 20:58:53 +0900

    コメントを追加

--- C6.py
+++ C6.py
@@ -1,8 +1,9 @@
-/***********************************************
-Designer:荒井*** 
-Date:2021.6.1*** 
-Purpose:課題情報管理部モジュール
-************************************************/
+#####################################################
+###***Designer:荒井
+###***Date:2021.6.18
+###***Purpose:C6コンポーネント
+#####################################################
+
 
 from app import taskConn
 import sqlite3
@@ -10,6 +11,20 @@ import uuid
 
 
 def taskQuerySub(userID,orderData)
+    """
+    機能概要    :指定日の課題の情報を検索する.
+
+    引数        :
+        userID(str)     :ユーザID
+        orderData(str)  :指定された日付
+
+    戻り値      
+        失敗:
+            文字列"failed"
+
+        成功:
+            newList(list):指定日の課題リスト
+    """
     cur=taskConn.Cursor()
     cur.execute('SELECT * FROM task.db WHERE orderDate='+str(due[0:10])+'AND userID='+str(userID))
     tempList=cur.fetchall()
@@ -22,8 +37,25 @@ def taskQuerySub(userID,orderData)
     
     return newList
 
-def taskEditSub(userID,due,need,title,taskID)
-    if a=1:
+def taskEditSub(userID,due,need,title,taskID):
+    """
+    機能概要    :課題の情報を更新する.
+
+    引数        :
+        userID(str)     :ユーザID
+        due(str)        :課題の締め切り時間
+        need(str)       :課題の必要時間
+        title(str)      :課題の名称
+        taskID(str)     :課題ID
+
+    戻り値      
+        失敗:
+            文字列"failed"
+
+        成功:
+            newTaskID   :新たな課題のID
+    """
+    if a==1:
         newTaskID=str(uuid.uuid4())
 
         cur=taskConn.Cursor()
@@ -32,7 +64,7 @@ def taskEditSub(userID,due,need,title,taskID)
 
         return newTaskID
 
-    elif a=2:
+    elif a==2:
         newTaskID=taskID
 
         cur=taskConn.Cursor()
@@ -53,7 +85,21 @@ def taskEditSub(userID,due,need,title,taskID)
 
         return newTaskID
 
-def taskQueryManySub(userID,orderData)
+def taskQueryManySub(userID,orderData):
+    """
+    機能概要    :指定日以降の課題の情報を検索する.
+
+    引数        :
+        userID(str)     :ユーザID
+        orderData(str)  :指定された日付
+
+    戻り値      
+        失敗:
+            文字列"failed"
+
+        成功:
+            newList(list):指定日の課題リスト
+    """
     cur=taskConn.Cursor()
     cur.execute('SELECT * FROM task.db WHERE orderDate>='+str(due[0:10])+'AND userID='+str(userID))
     tempList=cur.fetchall()
@@ -67,7 +113,20 @@ def taskQueryManySub(userID,orderData)
     return newList
 
 
-def taskQueryAllSub(userID)
+def taskQueryAllSub(userID):
+    """
+    機能概要    :ユーザの登録した全ての課題の情報を検索する.
+
+    引数        :
+        userID(str)     :ユーザID
+
+    戻り値      
+        失敗:
+            文字列"failed"
+
+        成功:
+            newList(list):指定日の課題リスト
+    """
     cur=taskConn.Cursor()
     cur.execute('SELECT * FROM task.db WHERE userID='+str(userID))
     tempList=cur.fetchall()
