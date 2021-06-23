4a5cb69554ff5cc77b6f9e4613abd66bb4b05005
al19056 <al19056@shibaura-it.ac.jp>
2021-06-23 14:44:56 +0900

    host=0.0.0.0,port=なし

--- app.py
+++ app.py
@@ -385,5 +385,5 @@ def errorPage(error):
 
 if __name__ == "__main__":
     app.run(
-        debug=False, port=50280, host="160.16.141.77", threaded=True
+        debug=False, host="0.0.0.0", threaded=True
     )  ###############host='0.0.0.0'とport=とthreaded=True,debug=Falseを指定する
