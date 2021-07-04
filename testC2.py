982577646e887c68c2237bb9ef475699f264a6f7
al19056 <al19056@shibaura-it.ac.jp>
2021-07-04 21:54:18 +0900

    不必要なファイルの削除

--- testC2.py
+++
@@ -1,9 +0,0 @@
-def authenticationProcessing(userID, password):
-    return "success"  # 成功
-    # return "failed"
-
-
-def addUser(userID, password):
-    # return "existed"  # ユーザID登録済み
-    # return "failed"
-    return "success"
